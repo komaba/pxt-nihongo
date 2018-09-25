f950f0c0e452540b9e3b7b10a19a586b172a9bd7
komaba <43165861+komaba@users.noreply.github.com>
2018-09-25 10:55:05 +0900

    ON and OFF

--- main.ts
+++ main.ts
@@ -1,7 +1,44 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+//% weight=70 icon="\u30A2" color=#EC7505 block="カタカナ"
+namespace katakana {
+    let scroll: number
+    const arr = [2550, 2550, 850, 850, 850, 850, 850, 1912, 850, 1912, 850, 1912, 850, 1912, 850, 850, 850, 850, 850, 54400, 2550, 2550, 850, 850, 850, 850, 850, 1912, 850, 1912, 850, 1912, 850, 1912, 850, 850, 850, 850, 850]
+    const arr2 = [2550, 2550, 850, 850, 850, 1912, 850, 850, 850, 850, 850, 1912, 850, 850, 850, 1912, 850, 1912, 850, 54400, 2550, 2550, 850, 850, 850, 1912, 850, 850, 850, 850, 850, 1912, 850, 850, 850, 1912, 850, 1912, 850]
+    //% blockId=on block="ON"
+    export function on(): void {
+        for (let i = 0, r = 0; i <= arr.length - 1; i++) {
+            // Generate flashing signal
+            if (i % 2 == 1) {
+                // LED turns off
+                control.waitMicros(arr[i])
+            } else {
+                // LED flashes at 38 kHz cycle
+                r = arr[i]
+                while (r > 26) {
+                    pins.digitalWritePin(DigitalPin.P0, 1)
+                    control.waitMicros(2)
+                    pins.digitalWritePin(DigitalPin.P0, 0)
+                    r = r - 26
+                }
+            }
+        }
+    }
+    //% blockId=on block="OFF"
+    export function off(): void {
+        for (let i = 0, r = 0; i <= arr2.length - 1; i++) {
+            // Generate flashing signal
+            if (i % 2 == 1) {
+                // LED turns off
+                control.waitMicros(arr2[i])
+            } else {
+                // LED flashes at 38 kHz cycle
+                r = arr2[i]
+                while (r > 26) {
+                    pins.digitalWritePin(DigitalPin.P0, 1)
+                    control.waitMicros(2)
+                    pins.digitalWritePin(DigitalPin.P0, 0)
+                    r = r - 26
+                }
+            }
+        }
+    }
+}
