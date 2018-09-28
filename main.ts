6f58f2ef2245e0f18ed425ce12ca0126b04b1fbb
komaba <43165861+komaba@users.noreply.github.com>
2018-09-28 11:06:21 +0900

    Undo

--- main.ts
+++ main.ts
@@ -1,45 +1,51 @@
 //% weight=70 icon="\u30A2" color=#EC7505 block="赤外線"
 namespace infrared {
     let scroll: number
-    const arr = [2550, 2550, 850, 850, 850, 850, 850, 1912, 850, 1912, 850, 1912, 850, 1912, 850, 850, 850, 850, 850, 54400, 2550, 2550, 850, 850, 850, 850, 850, 1912, 850, 1912, 850, 1912, 850, 1912, 850, 850, 850, 850, 850]
-    const arr2 = [2550, 2550, 850, 850, 850, 1912, 850, 850, 850, 850, 850, 1912, 850, 850, 850, 1912, 850, 1912, 850, 54400, 2550, 2550, 850, 850, 850, 1912, 850, 850, 850, 850, 850, 1912, 850, 850, 850, 1912, 850, 1912, 850]
+    const arr = [2550, 2550, 850, 850, 850, 850, 850, 1912, 850, 1912, 850, 1912, 850, 1912, 850, 850, 850, 850, 850]
+    const arr2 = [2550, 2550, 850, 850, 850, 1912, 850, 850, 850, 850, 850, 1912, 850, 850, 850, 1912, 850, 1912, 850]
     //% blockId=on block="ON"
     export function on(): void {
-        for (let i = 0, r = 0, time = 0; i <= arr.length - 1; i++) {
-            // Generate flashing signal
-            if (i % 2 == 1) {
-                // LED turns off
-                control.waitMicros(arr[i])
-            } else {
-                // LED flashes at 38 kHz cycle
-                time = input.runningTimeMicros()
-                pins.analogWritePin(AnalogPin.P0, 512)
-                pins.analogSetPeriod(AnalogPin.P0, 26.3)
-                if (input.runningTimeMicros() - time > arr[i]) {
-                    pins.analogWritePin(AnalogPin.P0, 0)
+        for (let j = 0; j < 2; j++){
+            for (let i = 0, r = 0, time = 0; i <= arr.length - 1; i++) {
+                // Generate flashing signal
+                if (i % 2 == 1) {
+                    // LED turns off
+                    control.waitMicros(arr[i])
+                } else {
+                    // LED flashes at 38 kHz cycle
+                    r = arr[i]
+                    while (r > 26) {
+                        pins.digitalWritePin(DigitalPin.P0, 1)
+                        control.waitMicros(2)
+                        pins.digitalWritePin(DigitalPin.P0, 0)
+                        r = r - 26
+                    }
                 }
             }
+            control.waitMicros(54400)    
         }
-        
         basic.showString("ON")
     }
     //% blockId=off block="OFF"
     export function off(): void {
-        for (let i = 0, r = 0; i <= arr2.length - 1; i++) {
-            // Generate flashing signal
-            if (i % 2 == 1) {
-                // LED turns off
-                control.waitMicros(arr2[i])
-            } else {
-                // LED flashes at 38 kHz cycle
-                r = arr2[i]
-                while (r > 26) {
-                    pins.digitalWritePin(DigitalPin.P0, 1)
-                    control.waitMicros(2)
-                    pins.digitalWritePin(DigitalPin.P0, 0)
-                    r = r - 26
+        for (let j = 0; j < 2; j++) {
+            for (let i = 0, r = 0, time = 0; i <= arr2.length - 1; i++) {
+                // Generate flashing signal
+                if (i % 2 == 1) {
+                    // LED turns off
+                    control.waitMicros(arr2[i])
+                } else {
+                    // LED flashes at 38 kHz cycle
+                    r = arr2[i]
+                    while (r > 26) {
+                        pins.digitalWritePin(DigitalPin.P0, 1)
+                        control.waitMicros(2)
+                        pins.digitalWritePin(DigitalPin.P0, 0)
+                        r = r - 26
+                    }
                 }
             }
+            control.waitMicros(54400)  
         }
         basic.showString("OFF")
     }
